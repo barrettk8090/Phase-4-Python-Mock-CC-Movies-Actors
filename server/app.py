cdb7c37b7a5794d14cf68e22de408e36a04202a8
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-01-15 10:04:10 -0700

    push working solution

--- server/app.py
+++ server/app.py
@@ -21,6 +21,141 @@ migrate = Migrate(app, db)
 db.init_app(app)
 api = Api(app)
 
+class Actors_Route(Resource):
+    def get(self):
+        all_actors = Actor.query.all()
+        actor_dict = []
+        for actor in all_actors:
+            actor_dict.append(actor.to_dict(rules = ('-credits',)))
+        return make_response(actor_dict, 200)
+    def post(self):
+        try:
+            data = request.get_json()
+            new_actor = Actor(
+                name= data["name"],
+                age= data["age"],
+            )
+            if new_actor:
+                db.session.add(new_actor)
+                db.session.commit()
+                return make_response(new_actor.to_dict(), 201)
+        except:
+            return make_response({"errors": ["validation errors"]}, 400)
+
+    
+api.add_resource(Actors_Route, '/actors')
+
+class Actor_By_ID(Resource):
+    def get(self, id):
+        single_actor = Actor.query.filter(Actor.id == id).first()
+        if single_actor:
+            return make_response(single_actor.to_dict(), 200)
+        else:
+            return({"error":"Actor not found"}, 404)
+    def patch(self, id):
+        single_actor = Actor.query.filter(Actor.id == id).first()
+        if single_actor:
+            try: 
+                data = request.get_json()
+                for attr in data:
+                    setattr(single_actor, attr, data[attr])
+                    db.session.add(single_actor)
+                    db.session.commit()
+                    return make_response(single_actor.to_dict(), 202)
+            except:
+                make_response({"errors": ["validation errors"]}, 400)
+        else:
+            return make_response({"error":"Actor not found"}, 404)
+    def delete(self, id):
+        single_actor = Actor.query.filter(Actor.id == id).first()
+        if single_actor:
+            db.session.delete(single_actor)
+            db.session.commit()
+            return make_response({}, 204)
+        else:
+            return make_response({"error":"Actor not found"}, 404)
+
+api.add_resource(Actor_By_ID, '/actors/<int:id>')
+
+class Movie_Route(Resource):
+    def get(self):
+        all_movies = Movie.query.all()
+        movie_dict = []
+        for movie in all_movies:
+            movie_dict.append(movie.to_dict(rules = ('-credits',)))
+        return make_response(movie_dict, 200)
+    def post(self):
+        try:
+            data = request.get_json()
+            new_movie = Movie(
+                image= data["image"],
+                title= data["title"],
+                genre= data["genre"],
+                rating= data["rating"],
+                description= data["description"],
+            )
+            db.session.add(new_movie)
+            db.session.commit()
+            return make_response(new_movie.to_dict(), 200)
+        except:
+            return make_response({"errors": ["validation errors"]})
+
+api.add_resource(Movie_Route, "/movies")
+
+class Credits_Route(Resource):
+    def get(self):
+        all_credits = Credit.query.all()
+        credit_dict = []
+        for credit in all_credits:
+            credit_dict.append(credit.to_dict(rules = ("-actors", "-movies")))
+        return make_response(credit_dict, 200)
+    def post(self):
+        try:
+            data = request.get_json()
+            new_credit = Credit(
+                actor_id = data["actor_id"],
+                movie_id = data["movie_id"],
+                role= data["role"]
+            )
+            db.session.add(new_credit)
+            db.session.commit()
+            return make_response(new_credit.to_dict(), 200)
+        except:
+            return make_response({"errors": ["validation errors"]})
+
+api.add_resource(Credits_Route, "/credits")
+
+class Credits_By_ID(Resource):
+    def get(self, id):
+        one_credit = Credit.query.filter(Credit.id == id).first()
+        if one_credit:
+            return make_response(one_credit.to_dict(), 200)
+        else:
+            return make_response({"error": "Credit not found"}, 404)
+    def patch(self, id):
+        one_credit = Credit.query.filter(Credit.id == id).first()
+        if one_credit:
+            try:
+                data = request.get_json()
+                for attr in data:
+                    setattr(one_credit, attr, data[attr])
+                db.session.add(one_credit)
+                db.session.commit()
+                return make_response(one_credit.to_dict(), 202)
+            except:
+                return make_response({"errors": ["validation errors"]}, 400)
+        else:
+            return make_response({"error": "Credit not found"}, 404)
+    def delete(self, id):
+        one_credit = Credit.query.filter(Credit.id == id).first()
+        if one_credit:
+            db.session.delete(one_credit)
+            db.session.commit()
+            return make_response({}, 204)
+        else:
+            return make_response({"errors": "Credit not found"})
+
+api.add_resource(Credits_By_ID, "/credits/<int:id>")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -18,9 +18,80 @@ db = SQLAlchemy(metadata=metadata)
 
 class Movie(db.Model, SerializerMixin):
     __tablename__='movie_table'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String)
+    title = db.Column(db.String)
+    genre = db.Column(db.String)
+    rating = db.Column(db.Integer)
+    description = db.Column(db.String)
+
+    credits = db.relationship("Credit", back_populates="movies")
+
+    serialize_rules = ('-credits.movies',)
+
+    @validates('rating')
+    def validate_rating(self, key, value):
+        if 1 <= value <= 10:
+            return value
+        else:
+            raise ValueError("A rating must be an integer between 1 and 10.")
+        
+
+    @validates('genre')
+    def validate_genre(self, key, value):
+        genre_types = [ "Action", "Comedy", "Drama", "Horror", "Romance", "Thriller", "Science Fiction", "Fantasy", "Mystery", "Adventure", "Crime", "Family", "Animation", "Documentary", "War" ]
+        if value in genre_types:
+            return value
+        else:
+            raise ValueError("Not a valid genre type.")
+
 
 class Actor(db.Model, SerializerMixin):
     __tablename__='actor_table'
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable = False)
+    age = db.Column(db.Integer)
+
+    credits = db.relationship("Credit", back_populates="actors")
+
+    serialize_rules = ('-credits.actors',)
+
+    @validates("name")
+    def validate_name(self, key, value):
+        if value:
+            return value
+        else:
+            raise ValueError("Not a valid name.")
+        
+    @validates("age")
+    def validate_age(self, key, value):
+        if value > 10:
+            return value
+        else:
+            raise ValueError("Not a valid age.")
+
+
+#JOIN TABLE - Columns and relationships
 class Credit(db.Model, SerializerMixin):
     __tablename__='credit_table'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String, nullable=False)
+
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie_table.id'), nullable=False)
+    actor_id = db.Column(db.Integer, db.ForeignKey('actor_table.id'), nullable=False)
+
+    movies = db.relationship("Movie", back_populates="credits")
+    actors = db.relationship("Actor", back_populates="credits")
+
+    serialize_rules = ('-movies.credits', 'actors.credits')
+
+    @validates("role")
+    def validate_role(self, key, value):
+        role_types = ["Performer", "Director", "Producor", "Playwright", "Lighting Design", "Sound Design", "Set Design"]
+        if value in role_types:
+            return value
+        else:
+            raise ValueError("Not a valid role type.")
